e566aa7c1164f681ca7a25524468027a0a30fca8
Mona Lisa <manjushamac@gmail.com>
2024-04-14 20:26:07 +0300

    Your commit message

---
+++ routes/product.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var router = express.Router();
+var ProductController = require('../controllers/product.controller');
+router.post('/add', ProductController.add);
+router.put('/update/:id', ProductController.update);
+router.delete('/delete/:id', ProductController.delete);
+router.get('/all', ProductController.all);
+router.get('/:id', ProductController.singleProduct);
+module.exports = router;
+
+
+
+// // http://localhost:9012/product/add 
+// {
+//     productId:
+//     name:
+//     price:
+//     description:
+//     imageURL: 
+// }
